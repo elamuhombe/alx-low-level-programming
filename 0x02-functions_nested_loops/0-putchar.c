8e1c1a61dca8b166413f18ea64258321920139e9
elaine <emuhombe@gmail.com>
2022-09-14 11:49:20 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *
+ * int _putchar - _putchar Entry point
+ *
+ * Description:Program that prints _putchar
+ *
+ * Return:Always 0 (Success)
+ */
+int _putchar
+{
+	putchar('_putchar');
+}
